e8e00e1392b6d4d1a6c712a0f4680838d787ea40
Kate Churhel <AngelEko@yandex.ru>
2018-06-28 15:57:35 +0300

    Оживляет модальные окна

--- js/main.js
+++ js/main.js
@@ -5,15 +5,19 @@ for (let i = 0; i < menuItem.length; i++) {
   let menu = menuItem[i].querySelectorAll(".submenu__link");
 
   for (let j = 0; j < menu.length; j++) {
-    menu[j].addEventListener("focus", () => {
-      console.log(menu[j]);
-      menuItem[i].style.opacity = "1";
-      menuItem[i].style.visibility = "visible";
-    })
-    menu[j].addEventListener("blur", () => {
-      menuItem[i].style.opacity = "";
-      menuItem[i].style.visibility = "";
-    })
+    menu[j].addEventListener("focus", function(i) {
+      return function() {
+        console.log(menu[j]);
+        menuItem[i].style.opacity = "1";
+        menuItem[i].style.visibility = "visible";
+      }
+    }(i));
+    menu[j].addEventListener("blur", function(i) {
+      return function() {
+        menuItem[i].style.opacity = "";
+        menuItem[i].style.visibility = "";
+      }
+    }(i));
   }
 }
 
@@ -22,57 +26,114 @@ for (let i = 0; i < menuItem.length; i++) {
 let catalogBtn = document.querySelectorAll(".catalog__btn");
 
 for (let i = 0; i < catalogBtn.length; i++) {
-  catalogBtn[i].addEventListener("focus", () => {
-    catalogBtn[i].parentNode.style.opacity = "1";
+  catalogBtn[i].addEventListener("focus", function() {
+    this.parentNode.style.opacity = "1";
   })
-  catalogBtn[i].addEventListener("blur", () => {
-    catalogBtn[i].parentNode.style.opacity = "";
+  catalogBtn[i].addEventListener("blur", function() {
+    this.parentNode.style.opacity = "";
   })
 }
 
 let catalogCompare = document.querySelectorAll(".catalog__compare");
 
 for (let i = 0; i < catalogBtn.length; i++) {
-  catalogCompare[i].addEventListener("focus", () => {
-    catalogCompare[i].parentNode.style.opacity = "1";
+  catalogCompare[i].addEventListener("focus", function() {
+    this.parentNode.style.opacity = "1";
   })
-  catalogCompare[i].addEventListener("blur", () => {
-    catalogCompare[i].parentNode.style.opacity = "";
+  catalogCompare[i].addEventListener("blur", function() {
+    this.parentNode.style.opacity = "";
   })
+}
+
+
+
+/**
+ * Cлайдер
+ * 
+ * @param  {Array} btns        Массив элементов управления слайдером
+ * @param  {Array} sliderItems Массив слайдов 
+ */
+function slider(btns, sliderItems) {
+  for (let i = 0; i < sliderItems.length; i++) {
+
+    btns[i].onclick = function(i) {
+      return function() {
+        for (let j = 0; j < sliderItems.length; j++) {
+          btns[j].classList.remove("active");
+          sliderItems[j].classList.add("visually-hidden");
+        }
+        btns[i].classList.add("active");
+        sliderItems[i].classList.remove("visually-hidden");
+      }
+    }(i);
+  }
 }
 
 
 // Слайдер лучших товаров
 let sliderBtn = document.querySelectorAll(".slider__btn");
 let sliderItem = document.querySelectorAll(".slider__slide");
-
-for (let i = 0; i < sliderItem.length; i++) {
-
-  sliderBtn[i].onclick = () => {
-    for (let j = 0; j < sliderItem.length; j++) {
-      sliderBtn[j].classList.remove("active");
-      sliderItem[j].classList.add("visually-hidden");
-    }
-    sliderBtn[i].classList.add("active");
-    sliderItem[i].classList.remove("visually-hidden");
-
-  }
-}
+slider(sliderBtn, sliderItem);
 
 
 // Слайдер услуг
 let servicesBtn = document.querySelectorAll(".services__btn");
 let servicesItem = document.querySelectorAll(".services__slide");
+slider(servicesBtn, servicesItem);
 
-for (let i = 0; i < servicesItem.length; i++) {
 
-  servicesBtn[i].onclick = () => {
-    for (let j = 0; j < servicesItem.length; j++) {
-      servicesBtn[j].classList.remove("active");
-      servicesItem[j].classList.add("visually-hidden");
-    }
-    servicesBtn[i].classList.add("active");
-    servicesItem[i].classList.remove("visually-hidden");
 
+
+// Оживляем модальные окна
+let modalWriteUs = document.querySelector(".modal-write");
+let modalMap = document.querySelector(".modal-map");
+let contactImage = document.querySelector(".contacts__image");
+let contactBtn = document.querySelector(".contacts__btn");
+let closeModalBtn = document.querySelectorAll(".madal__close");
+let modalSubmitBtn = modalWriteUs.querySelector(".modal-write__btn");
+let modalWriteUsInput = modalWriteUs.querySelectorAll("input[required]");
+
+for (let i = 0; i < closeModalBtn.length; i++) {
+  closeModalBtn[i].addEventListener("click", function(evt){
+    evt.preventDefault();
+    this.parentNode.classList.remove("modal-show");
+    this.parentNode.classList.remove("modal-error");
+  })
+};
+
+contactBtn.addEventListener("click", function(evt){
+  evt.preventDefault();
+  modalWriteUs.classList.add("modal-show");
+  modalWriteUs.querySelector("input").focus();
+});
+
+modalSubmitBtn.addEventListener("click", function(evt){
+  let isValue = true;
+  for (var i = 0; i < modalWriteUsInput.length; i++) {
+    if(!modalWriteUsInput[i].value) isValue=false;
   }
-}
+  if(!isValue){
+    evt.preventDefault();
+    modalWriteUs.classList.remove("modal-error");
+   modalWriteUs.offsetWidth = modalWriteUs.offsetWidth;
+    modalWriteUs.classList.add("modal-error");
+    console.log("Error");
+  }
+});
+
+contactImage.addEventListener("click", function(evt){
+  evt.preventDefault();
+  modalMap.classList.add("modal-show");
+})
+
+
+
+
+
+
+
+
+
+
+
+
